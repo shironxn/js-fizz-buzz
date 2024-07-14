8cfb3baf8ec4d838d688c9c3f92d7df20f547edd
shironxn <qshiro69@gmail.com>
2024-07-14 11:08:49 +0700

    chore: update fizzbuzz logic and docss

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const fizz = 3;
 const buzz = 5;
 
 function run(num) {
-  if (num % fizz === 0) {
+  if (num % fizz === 0 && num % buzz === 0) {
+    return "fizzbuzz";
+  } else if (num % fizz === 0) {
     return "fizz";
   } else if (num % buzz === 0) {
     return "buzz";
--- readme.md
+++ readme.md
@@ -1,6 +1,11 @@
 ## Usage
 
+1. Clone this repo:
+```bash
+git clone https://github.com/shironxn/js-fizz-buzz
+```
 
+2. Run:
 ```bash
 npm start
 ```
